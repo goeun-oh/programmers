364e761d18141b65f9398e22fa05ed79c3e2fe9c
goeun-oh <103434703+goeun-oh@users.noreply.github.com>
2025-02-10 14:00:17 +0900

    [level 0] Title: 모음 제거, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -1,10 +1,7 @@
 def solution(my_string):
     answer = ''
-    list0=['a','e','i','o','u']
-    my_string=list(my_string)
-    for s in list0:
-        while(s in my_string):
-            my_string.remove(s)
-    for i in range(len(my_string)):    
-        answer += my_string[i]
+    A=['a','e','i','o','u']
+    for s in A:
+            my_string=my_string.replace(s, '')
+    answer=my_string
     return answer
