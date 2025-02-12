b46d83e2ac2a1a0ce52720c022a3880d513643a2
goeun-oh <103434703+goeun-oh@users.noreply.github.com>
2025-02-12 15:07:06 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -2,6 +2,5 @@ def solution(my_string, num1, num2):
     answer = ''
     my_string=list(my_string)
     my_string[num1],my_string[num2] = my_string[num2],my_string[num1]
-    for i in my_string:
-        answer += i
+    answer="".join(my_string)
     return answer
